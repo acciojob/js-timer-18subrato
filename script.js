fb939a947e4c7888ce25a7c3cdd38f8e04e397c7
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-19 16:02:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// write js code here if required
+let time = document.querySelector('#timer');
+
+let date = new Date();
+
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDay();
+
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+
+time.innerText = `${day}/${month}/${year}, ${hour}:${minute}:${second}`;
